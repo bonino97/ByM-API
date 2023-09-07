48e3e3b702696490ae4601e110799a17db119b04
bautibadino <bautistabadino@gmail.com>
2023-09-07 18:55:58 +0200

    advances

---
+++ src/models/Service.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const serviceSchema = new Schema({
+    model: String,
+    seller: Schema.Types.ObjectId, ref: 'User',
+    patent: String,
+    address: String,
+    oilFilter:String,
+    airFilter:String,
+    fuelFilter: String,
+    oilType: String,
+    orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
